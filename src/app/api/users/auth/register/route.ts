970e5b5f4b7a9446908dab59c51e593d78ae63d9
leooc1 <leonardoocvrb@gmail.com>
2023-10-11 16:31:42 -0300

    replace async functions

--- src/app/api/users/auth/register/route.ts
+++ src/app/api/users/auth/register/route.ts
@@ -15,7 +15,7 @@ type reqBory = {
 
 export async function POST(req: NextRequest) {
     const { NOME, EMAIL, SENHA }: reqBory = await req.json()
-    const DATA = utils.atualDate()
+    const DATA = await utils.atualDate()
     const hashPassword = await utils.hash(SENHA)
     const result: Result = await modelUsers.register(NOME, EMAIL, hashPassword, DATA)
     return NextResponse.json(result.value, { status: result.status })
